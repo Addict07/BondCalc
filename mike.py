aeb5a9640ded33d82bc93fd6fb605ae9f1b95f8a
Michael Nketia <addictedchrysler@gmail.com>
2023-03-30 15:03:28 +0000

    first commit

---
+++ mike.py
@@ -0,0 +1,18 @@
+e =  "2025-04-07_second"
+d = "2023-03-06_1"
+
+mat, id = e.split("_")
+print(mat)
+print(id)
+print(type(id))
+
+m, ident = d.split('_')
+print(m)
+print(ident)
+print(type(ident))
+
+# <option value="2023-03-06">6 March 2023</option>
+#                 <option value="2023-04-17">17 April 2023</option>
+
+
+
